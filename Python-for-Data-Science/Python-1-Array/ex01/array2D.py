865974f0cf8812e59cb425c27afdafcb53944e78
Loris Puchol <lorispuchol@gmail.com>
2024-03-06 11:11:06 +0100

    ex01

--- Python-for-Data-Science/Python-1-Array/ex01/array2D.py
+++ Python-for-Data-Science/Python-1-Array/ex01/array2D.py
@@ -10,15 +10,16 @@ def slice_me(family: list, start: int, end: int) -> list:
     if len(sublist_lengths) != 1:
         print("Error: All sublists in 'family' must have the same length")
         return []
-    Arr = np.array(family)
-    print("My shape is :", Arr.shape)
-    truncated = Arr[start:end]
-    print("My new shape is :", Arr.shape)
+
+    arr = np.array(family)
+    print("My shape is :", arr.shape)
+    truncated = arr[start:end]
+    print("My new shape is :", arr.shape)
     return truncated
 
 def main():
     family = [
-        [1.80, 78.4],
+        [1.80, 78.4, 0],
         [2.15, 102.7],
         [2.10, 98.5],
         [1.88, 75.2]
